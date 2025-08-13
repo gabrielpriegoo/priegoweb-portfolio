ba0fa7a058605a3fb8bf6d4dd33f48313d5bc970
gabrielpriegoo <gabrielvpriego@gmail.com>
2025-08-13 12:09:33 -0300

    feat: :rocket: initial commit, with configs setup tailwind, vite, shadcn and aceternity ui.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ThreeDCardDemo } from "./components/ui/three-card-demo";
+
+function App() {
+  return (
+    <>
+      <div>
+        <ThreeDCardDemo />
+      </div>
+    </>
+  );
+}
+
+export default App;
